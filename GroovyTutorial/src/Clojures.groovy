3a7225a781a713349833e253294f1819149722ec
Asher Sterkin <asterkin@cisco.com>
2014-01-08 13:29:11 +0200

    Some code added

--- GroovyTutorial/src/Clojures.groovy
+++ GroovyTutorial/src/Clojures.groovy
@@ -5,6 +5,10 @@ class Clojures {
 		def square = { it * it }
 		
 		println square(9)
+		println ([ 1, 2, 3, 4 ].collect(square))
+		
+		def printMapClosure = { key, value -> println key + "=" + value }
+		[ "Yue" : "Wu", "Mark" : "Williams", "sudha" : "Kumari" ].each(printMapClosure)
 	}
 
 }
