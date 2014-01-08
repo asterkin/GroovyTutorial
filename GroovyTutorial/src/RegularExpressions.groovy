e31095470f84ec6d8fbde004671ca57335d76aa7
Asher Sterkin <asterkin@cisco.com>
2014-01-08 14:47:02 +0200

    Mathing and Not Mathcing groups added

--- GroovyTutorial/src/RegularExpressions.groovy
+++ GroovyTutorial/src/RegularExpressions.groovy
@@ -5,6 +5,34 @@ class RegularExpressions {
 		patterns()
 		regular_expressions()
 		operators()
+		matching_groups()
+		not_matching_groups();
+	}
+
+	private static not_matching_groups() {
+		def names = [
+			"Graham James Edward Miller",
+			"Andrew Gregory Macintyre",
+			"No MiddleName"
+		]
+
+		def printClosure = {
+			def matcher = (it =~ /(.*?)(?: .*)* (.*)/);  // notice the non-matching group in the middle
+			if (matcher.matches())
+				println(matcher[0][2]+", "+matcher[0][1]);
+		}
+		names.each(printClosure)
+	}
+
+	private static matching_groups() {
+		def locationData = "Liverpool, England: 53d 25m 0s N 3d 0m 0s"
+		def myRegularExpression = /([a-zA-Z]+), ([a-zA-Z]+): ([0-9]+). ([0-9]+). ([0-9]+). ([A-Z]) ([0-9]+). ([0-9]+). ([0-9]+)./
+		def matcher = ( locationData =~ myRegularExpression )
+		println matcher[0]
+		if (matcher.matches()) {
+			println(matcher.getCount()+ " occurrence of the regular expression was found in the string.");
+			println(matcher[0][1] + " is in the " + matcher[0][6] + " hemisphere. (According to: " + matcher[0][0] + ")")
+		}
 	}
 
 	private static operators() {
